4a826fc3864a78d8052366ee97b0792e31bc070c
Faran Saeed <faransaeed@rocketmail.com>
2021-04-01 20:23:52 +0500

    added start and reset button

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,13 @@
   let gameboard = {
     gameboardArr: [],
     count: 0,
+    buttonValue: '',
     init: function () {
       this.cacheDOM();
       this.bind();
     },
     cacheDOM: function () {
+      this.main = document.querySelector('main');
       this.button = document.querySelector('button');
       this.options = document.querySelector('#gameboard-section');
       this.option = function (value) {
@@ -14,19 +16,34 @@
       };
     },
     render: function () {
-      this.option(this.optionID).innerText = this.gameboardArr[
-        this.gameboardArr.length - 1
-      ];
+      if (this.buttonValue === 'Start') {
+        this.button.innerText = 'Reset';
+        this.buttonValue = 'Reset';
+      } else if (this.buttonValue === 'Reset') {
+        this.option(this.optionID).innerText = this.gameboardArr[
+          this.gameboardArr.length - 1
+        ];
+      }
     },
     takeTurn: function (event) {
-      if (event.target && !event.target.innerText) {
+      if (
+        event.target.nodeName === 'BUTTON' &&
+        event.target.innerText === 'Start'
+      ) {
+        this.buttonValue = 'Start';
+        this.render();
+      } else if (
+        this.buttonValue === 'Reset' &&
+        event.target.className === 'option' &&
+        !event.target.innerText
+      ) {
         this.choice();
         this.optionID = event.target.id;
         this.render();
       }
     },
     bind: function () {
-      this.options.addEventListener('click', this.takeTurn.bind(this));
+      this.main.addEventListener('click', this.takeTurn.bind(this));
     },
     choice: function () {
       if (this.count % 2 === 0) {
