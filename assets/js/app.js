5be02fda91f64c4ada187523a83d1fe724329458
Faran Saeed <faransaeed@rocketmail.com>
2021-04-03 19:08:29 +0500

    Created the game with two players

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,59 +1,95 @@
 (() => {
-  let gameboard = {
+  const gameboard = {
     gameboardArr: [],
-    count: 0,
-    buttonValue: '',
+    turnCount: 0,
     init: function () {
       this.cacheDOM();
       this.bind();
     },
     cacheDOM: function () {
-      this.main = document.querySelector('main');
+      this.para = document.querySelector('p');
       this.button = document.querySelector('button');
-      this.options = document.querySelector('#gameboard-section');
+      this.gameboard = document.querySelector('#gameboard-section');
       this.option = function (value) {
         return document.querySelector(`[data-index="${value}"]`);
       };
     },
     render: function () {
-      if (this.buttonValue === 'Start') {
-        this.button.innerText = 'Reset';
-        this.buttonValue = 'Reset';
-      } else if (this.buttonValue === 'Reset') {
-        this.option(this.optionIndex).innerText = this.gameboardArr[
-          this.gameboardArr.length - 1
-        ];
+      this.option(this.optionIndex).innerText = this.gameboardArr[
+        this.gameboardArr.length - 1
+      ];
+      if (Player1.check(Player1.Array, combinations)) {
+        this.para.innerText = `${Player1.name} wins!`;
+      } else if (Player2.check(Player2.Array, combinations)) {
+        this.para.innerText = `${Player2.name} wins!`;
+      } else if (this.turnCount > 8 && !this.para.innerText.includes('wins')) {
+        this.para.innerText = 'Its a Tie!';
       }
     },
-    takeTurn: function (event) {
-      if (
-        event.target.nodeName === 'BUTTON' &&
-        event.target.innerText === 'Start'
-      ) {
-        this.buttonValue = 'Start';
-        this.render();
-      } else if (
-        this.buttonValue === 'Reset' &&
-        event.target.className === 'option' &&
-        !event.target.innerText
-      ) {
-        this.choice();
+    gameController: function (event) {
+      if (!event.target.innerText && !this.para.innerText.includes('wins')) {
+        this.playerMove(event);
         this.optionIndex = event.target.dataset.index;
         this.render();
       }
     },
-    bind: function () {
-      this.main.addEventListener('click', this.takeTurn.bind(this));
+    reset: function () {
+      document.querySelectorAll('.option').forEach((item) => {
+        item.innerText = '';
+      });
+      this.turnCount = 0;
+      this.para.innerText = '';
+      this.optionIndex = '';
+      this.gameboardArr = [];
+      Player1.Array = [];
+      Player2.Array = [];
     },
-    choice: function () {
-      if (this.count % 2 === 0) {
+    bind: function () {
+      this.gameboard.addEventListener('click', this.gameController.bind(this));
+      this.button.addEventListener('click', this.reset.bind(this));
+    },
+    playerMove: function (event) {
+      if (this.turnCount % 2 === 0) {
         this.gameboardArr.push('X');
+        Player1.Array.push(parseInt(event.target.dataset.index));
       } else {
         this.gameboardArr.push('O');
+        Player2.Array.push(parseInt(event.target.dataset.index));
       }
-      this.count++;
+      this.turnCount++;
     },
   };
+
+  const combinations = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  const player = (number) => {
+    const name = prompt(
+      `Please enter player ${number}'s name`,
+      `Player${number}`
+    );
+    let Array = [];
+
+    const check = function (target, combinations) {
+      return combinations.some((element) => compare(target, element));
+    };
+    const compare = function (target, element) {
+      return element.every((value) => target.includes(value));
+    };
+
+    return { name, Array, check };
+  };
+
+  const Player1 = player(1);
+  const Player2 = player(2);
 
   gameboard.init();
 })();
